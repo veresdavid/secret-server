171ac8a647e9da19b06b500e7234353a8afb1a70
Veres Dávid <veres.david11@gmail.com>
2019-07-29 21:30:05 +0200

    Modified database connection URI

--- src/database.ts
+++ src/database.ts
@@ -3,8 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const uri: string = "mongodb://localhost/secret";
-// const uri: string = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABSE}`;
+const uri: string = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_DATABSE}?retryWrites=true&w=majority`;
 
 export default async function connectDb() {
 
